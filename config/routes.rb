84d05aaacdd6d9a4ee3b28d9a1f5a7ae002bbf15
Guggs57 <n_guggenbuhl@hotmail.com>
2025-07-15 22:39:23 +0200

    adding bcrypt on gemfile admin side done

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # ✅ Gestion de la session admin (login/logout)
+  resource :session, only: [:new, :create, :destroy]
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # ✅ Espace admin sécurisé
+  namespace :admin do
+    get "products/index"
+    get "products/new"
+    get "products/create"
+    get "products/edit"
+    get "products/update"
+    get "products/destroy"
+    resources :products
+  end
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # ✅ Page publique d’accueil
+  root "home#index"
 end
