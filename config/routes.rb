7685efeb8eb34794f321a15f8809b5e7a633d0f3
Guggs57 <n_guggenbuhl@hotmail.com>
2025-07-16 22:28:01 +0200

    controller home product + views ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
-  # ✅ Gestion de la session admin (login/logout)
+  get "home/index"
+  # ✅ Session admin (login/logout)
   resource :session, only: [:new, :create, :destroy]
 
   # ✅ Espace admin sécurisé
   namespace :admin do
-    get "products/index"
-    get "products/new"
-    get "products/create"
-    get "products/edit"
-    get "products/update"
-    get "products/destroy"
     resources :products
   end
 
-  # ✅ Page publique d’accueil
+  # ✅ Vitrine publique (utilisateurs)
+  resources :products, only: [:index, :show]
+
+  # ✅ Page d’accueil publique
   root "home#index"
 end
